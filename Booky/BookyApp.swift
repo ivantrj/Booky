938a604d1867261d71736de91e0b9d5038562959
Ivan Trajanovski ruwido <ivan.trajanovski@ruwido.com>
2024-05-28 15:51:26 +0200

    Initial Commit

---
+++ Booky/BookyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookyApp.swift
+//  Booky
+//
+//  Created by Ivan Trajanovski  on 28.05.24.
+//
+
+import SwiftUI
+
+@main
+struct BookyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Booky/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Booky
+//
+//  Created by Ivan Trajanovski  on 28.05.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
