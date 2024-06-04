b072239e89bc4454d132ded8e2b2d169f44f78fa
Ivan Trajanovski <ivantrajanovski@gmail.com>
2024-06-04 14:12:33 +0200

    add plus button on tab view

--- Booky/ContentView.swift
+++ Booky/ContentView.swift
@@ -8,20 +8,37 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var isShowingAddBookSheet = false
+    
     var body: some View {
-        TabView {
-            InboxView()
-                .tabItem {
-                    Image(systemName: "tray.full")
-                    Text("Inbox")
-                }
-
-            LibraryView()
-                .tabItem {
-                    Image(systemName: "book")
-                    Text("Library")
-                }
+        ZStack(alignment: .bottom) {
+            TabView {
+                InboxView()
+                    .tabItem {
+                        Image(systemName: "tray.full")
+                        Text("Inbox")
+                    }
+                
+                LibraryView()
+                    .tabItem {
+                        Image(systemName: "book")
+                        Text("Library")
+                    }
+            }
+            Button {
+                isShowingAddBookSheet = true
+            } label: {
+                Image(systemName: "plus")
+                    .resizable()
+                    .tint(Color.white)
+                    .frame(width: 30, height: 30)
+            }
+            .frame(width: 60, height: 60)
+            .background(Color.blue)
+            .clipShape(Circle())
         }
-       
+        .sheet(isPresented: $isShowingAddBookSheet) {
+            AddBookSheetView()
+        }
     }
 }
